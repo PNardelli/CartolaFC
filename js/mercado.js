0488fe0c72b69ed533a51c9089d6853d4115975f
Pedro Nardelli <pedronardelli@MacBook-Pro-de-Nardelli.local>
2021-10-20 13:44:26 -0300

    rodada 27 e tabela do campeonato

--- js/mercado.js
+++ js/mercado.js
@@ -1,9 +1,11 @@
 const mercado = document.querySelector("#open-close")
-const icone = document.getElementsByClassName
+console.log(mercado)
+
+var rodada = 27
 
 
-var horaClose = 'Data:16' + ' Horas: ' + '18' + ':' + '30';
-var dataClose = '16/10/2021'
+var horaClose = 'Data:23' + ' Horas: ' + '18' + ':' + '30';
+var dataClose = '23/10/2021'
 var data = new Date()
 
 var dia = String(data.getDate()). padStart(2, '0');
@@ -15,9 +17,16 @@ var horaAtual = 'Data:' + dia + ' Horas: ' + hora + ':' + min;
 
 dataAtual = dia + '/' + mes + '/' + ano;
 
+if (dataClose > dataAtual) {
+    rodada = rodada + 1
+    console.log('resultado if: ' + rodada)
+}else{
+    console.log('resultado else: ')
+}
 
 console.log('Hora do Fechamento: ' + horaClose + '------> Hora Atual: ' + horaAtual)
 console.log('Data do Fechamento: ' + dataClose + '------> Data Atual: ' + dataAtual)
+
 
 if (horaClose <= horaAtual && dataClose <= dataAtual) {
     console.log('Teste OK! -> ' + dataClose + '-> ' + dataAtual)
@@ -28,5 +37,6 @@ mercadoCor.classList.remove("bg-success")
 mercadoCor.classList.add("bg-danger")
 
 } else {
-    console.log('Mercado Aberto!')   
+    mercado.innerHTML = 'Mercado Aberto Rodada ' + rodada
+    console.log('Mercado Aberto Rodada ' + rodada + '!')   
 };
