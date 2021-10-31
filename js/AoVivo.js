2af43ab6b778c8e0772d6f5aa71a2bc5494391fe
Pedro Nardelli <pedronardelli@MacBook-Pro-de-Nardelli.local>
2021-10-31 13:09:33 -0300

    add jogos ao vivo

--- js/AoVivo.js
+++ js/AoVivo.js
@@ -2,6 +2,13 @@ function trocarJogoAoVivo() {
     const jogo1 = document.querySelector("#ao-vivo1").className = "ao-vivo"
     const jogo2 = document.querySelector("#ao-vivo2").className = "ao-vivo"
     const jogo3 = document.querySelector("#ao-vivo3").className = "ao-vivo"
+    const jogo4 = document.querySelector("#ao-vivo1").className = "ao-vivo"
+    const jogo6 = document.querySelector("#ao-vivo2").className = "ao-vivo"
+    const jogo6 = document.querySelector("#ao-vivo3").className = "ao-vivo"
+    const jogo7 = document.querySelector("#ao-vivo1").className = "ao-vivo"
+    const jogo8 = document.querySelector("#ao-vivo2").className = "ao-vivo"
+    const jogo9 = document.querySelector("#ao-vivo3").className = "ao-vivo"
+    const jogo10 = document.querySelector("#ao-vivo3").className = "ao-vivo"
   }
 
   //trocarJogoAoVivo()
