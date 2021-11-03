2063da1675153f598309f95485409e6f686cce24
Pedro Nardelli <pedronardelli@MacBook-Pro-de-Nardelli.local>
2021-11-03 09:54:24 -0300

    Ajustado a tabela da rodada 29

--- js/rodada.js
+++ js/rodada.js
@@ -6,3 +6,7 @@ function alterarRodada27() {
 function alterarRodada28() {
     document.getElementById("tabela-serie-A").src="img/tabelaBrasileirao/TabelaBrasileirao28.png"
 }
+
+function alterarRodada29() {
+    document.getElementById("tabela-serie-A").src="img/tabelaBrasileirao/TabelaBrasileirao29.png"
+}
