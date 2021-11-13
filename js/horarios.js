3a45fb7b97d25b48d5f7a792202d73d83dda4412
Pedro Nardelli <pedronardelli@MacBook-Pro-de-Nardelli.local>
2021-11-13 18:01:07 -0300

    jogos rodada 32

--- js/horarios.js
+++ js/horarios.js
@@ -8,14 +8,40 @@ var horarioAs16 = 16
   var minAs15 = 15
   var minAs00 = 00
   var minAs30 = 30
-  var dataDosJogos = "03/11/2021"
+  var dataDosJogosSabado = "13/11/2021"
+  var dataDosJogosDomingo = '14/11/2021'
 
 
   var resultHora = false
   var resultMin = false
- 
+  var resultData = false
+
+  if(dataDosJogosSabado == dataAtual){
+    resultData = true
+  }
+
+  console.log(hora+min)
 
 
-  console.log("Hora: " + hora)
-  console.log("Minuto: " + min)
-  console.log("Data Atual: " + dataAtual)
+    if (resultData == true && hora+min >= 1700 && hora+min < 1900) {
+        trocarJogo1();    
+        console.log("Jogo 1 ao vivo")  
+    }
+
+    if (resultData == true && hora + min >= 1830 && hora + min <= 2030) {
+      trocarJogo2();
+      console.log("Jogo 2 ao vivo")
+
+    }
+
+    if (resultData == true && hora + min >= 1900 && hora + min <= 2100) {
+      trocarJogo3();
+      trocarJogo4();
+
+      console.log("Jogo 3 e 4 ao vivo")
+    }
+
+    if (resultData == true && hora + min >= 2100 && hora + min <= 2300) {
+      trocarJogo5();
+      console.log("Jogo 5 ao vivo")
+    }
